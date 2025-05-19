6f73e7be8ccc4645a0be01d33b20ca689e281d88
Luisa Vargas <luisavdornelles@gmail.com>
2025-05-19 18:02:19 -0400

    Update App.jsx
    - Create Card component
    - Replace Vite code
    - Display cards in loop

--- monsters-rolodex/src/App.jsx
+++ monsters-rolodex/src/App.jsx
@@ -1,48 +1,31 @@
-import { useState } from "react"; // useState is a hook that allows you to add state to functional components
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+// import { useState } from "react"; // useState is a hook that allows you to add state to functional components
 import "./App.css";
+
+const Card = (props) => {
+    return <div>{props.name}</div>;
+};
 
 function App() {
     // https://react.dev/reference/react/useState
     // useState is a hook that allows you to add state to functional components
     // It returns an array with two elements: the current state value and a function to update it
-    // The initial state is set to 0 in this case
     // set functions, like setSomething(nextState):
     // The set function returned by useState lets you update the state to a different value and trigger a re-render.
     // You can pass the next state directly, or a function that calculates it from the previous state
-    const [count, setCount] = useState(0);
 
     // <> is a shorthand for <React.Fragment>
     // React.Fragment is a component that allows you to group multiple elements without adding extra nodes to the DOM
     // It is useful when you want to return multiple elements from a component without wrapping them in a div or other element
     // It could be replaced with <React.Fragment> and </React.Fragment> or <Fragment> and </Fragment>
+
+    const arr = ["a", "b", "c"];
+
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} className="logo" alt="Vite logo" />
-                </a>
-                <a href="https://react.dev" target="_blank">
-                    <img
-                        src={reactLogo}
-                        className="logo react"
-                        alt="React logo"
-                    />
-                </a>
-            </div>
-            <h1>Vite + React</h1>
-            <div className="card">
-                <button onClick={() => setCount((count) => count + 1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/App.jsx</code> and save to test HMR
-                </p>
-            </div>
-            <p className="read-the-docs">
-                Click on the Vite and React logos to learn more
-            </p>
+            {arr.map((name, idx) => {
+                console.log(name, idx);
+                return <Card key={`${name}-${idx}`} name={name}></Card>;
+            })}
         </>
     );
 }
