7668844d9766ee1697c39c1dd51d42396c949c9c
Luisa Vargas <luisavdornelles@gmail.com>
2025-05-19 18:16:55 -0400

    Update App.jsx
    - using useState in an example

--- monsters-rolodex/src/App.jsx
+++ monsters-rolodex/src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from "react"; // useState is a hook that allows you to add state to functional components
+import { useState } from "react"; // useState is a hook that allows you to add state to functional components
 import "./App.css";
 
 const Card = (props) => {
@@ -12,20 +12,25 @@ function App() {
     // set functions, like setSomething(nextState):
     // The set function returned by useState lets you update the state to a different value and trigger a re-render.
     // You can pass the next state directly, or a function that calculates it from the previous state
+    const [arr, setArr] = useState(["a", "b", "c"]);
 
     // <> is a shorthand for <React.Fragment>
     // React.Fragment is a component that allows you to group multiple elements without adding extra nodes to the DOM
     // It is useful when you want to return multiple elements from a component without wrapping them in a div or other element
     // It could be replaced with <React.Fragment> and </React.Fragment> or <Fragment> and </Fragment>
-
-    const arr = ["a", "b", "c"];
-
     return (
         <>
             {arr.map((name, idx) => {
-                console.log(name, idx);
                 return <Card key={`${name}-${idx}`} name={name}></Card>;
             })}
+
+            <button
+                onClick={() => {
+                    setArr((arr) => [...arr, "d"]);
+                }}
+            >
+                add letter
+            </button>
         </>
     );
 }
