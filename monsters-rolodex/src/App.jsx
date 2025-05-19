4ba1406cef83b59ca9ea3672f07a2b88b2479645
Luisa Vargas <luisavdornelles@gmail.com>
2025-05-19 18:59:06 -0400

    Update App.jsx
    - Adding search feature

--- monsters-rolodex/src/App.jsx
+++ monsters-rolodex/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
     // The set function returned by useState lets you update the state to a different value and trigger a re-render.
     // You can pass the next state directly, or a function that calculates it from the previous state
     const [monsters, setMonsters] = useState([]);
+    const [filteredMonsters, setFilteredMonsters] = useState([]);
 
     // useEffect is a hook that allows you to perform side effects in functional components
     // if an empty array is passed, it will run only once after the first render
@@ -17,12 +18,25 @@ function App() {
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/users")
             .then((response) => response.json())
-            .then((data) => setMonsters(data));
+            .then((data) => {
+                setMonsters(data);
+                setFilteredMonsters(data);
+            });
     }, []);
 
     return (
         <>
-            <CardList monsters={monsters}></CardList>
+            <input
+                type="search"
+                onChange={(event) => {
+                    const searchString = event.target.value.toLocaleLowerCase();
+                    const filteredMonsters = monsters.filter((monster) =>
+                        monster.name.toLocaleLowerCase().includes(searchString)
+                    );
+                    setFilteredMonsters(filteredMonsters);
+                }}
+            />
+            <CardList monsters={filteredMonsters}></CardList>
         </>
     );
 }
