3ab4cf412a0b63f451729846dd522256a016d62f
Luisa Vargas <luisavdornelles@gmail.com>
2025-05-19 18:20:52 -0400

    Update App.jsx
    - useEffect basics

--- monsters-rolodex/src/App.jsx
+++ monsters-rolodex/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"; // useState is a hook that allows you to add state to functional components
+import { useState, useEffect } from "react"; // useState is a hook that allows you to add state to functional components
 import "./App.css";
 
 const Card = (props) => {
@@ -13,6 +13,12 @@ function App() {
     // The set function returned by useState lets you update the state to a different value and trigger a re-render.
     // You can pass the next state directly, or a function that calculates it from the previous state
     const [arr, setArr] = useState(["a", "b", "c"]);
+
+    // useEffect is a hook that allows you to perform side effects in functional components
+    // if an empty array is passed, it will run only once after the first render
+    useEffect(() => {
+        console.log("useEffect");
+    }, []);
 
     // <> is a shorthand for <React.Fragment>
     // React.Fragment is a component that allows you to group multiple elements without adding extra nodes to the DOM
