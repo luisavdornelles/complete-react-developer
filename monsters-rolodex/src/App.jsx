a709c8e8e84e346502849d11c9526857c4239ba3
Luisa VD <35203863+luisavdornelles@users.noreply.github.com>
2025-05-25 11:16:39 -0400

    Project Monsters Rolodex (#1)
    * setting up project with Vite
    
    * Update main.jsx
    
    file overview
    
    * Update App.jsx
    
    File overview
    
    * useState overview
    
    * Update eslint.config.js
    
    "react/prop-types": "off"
    
    * Update App.jsx
    
    - Create Card component
    - Replace Vite code
    - Display cards in loop
    
    * Update App.jsx
    
    - using useState in an example
    
    * Update App.jsx
    
    - useEffect basics
    
    * Update App.jsx
    
    - Update Card component
    - populating monsters array with mock data
    
    * Update App.jsx
    
    - adding the monsters images
    
    * Folder structure
    
    - Creating folder structure
    - Adding card list styling
    - Removing Vite css
    
    * Update App.jsx
    
    - Adding search feature
    
    * Update index.html
    
    - Update page title and favicon
    
    * Creating search-box component
    
    * Updating styles with finishing touches
    
    * Update README.md
    
    * Create sonar-project.properties
    
    * Create build.yml

---
+++ monsters-rolodex/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react"; // useState is a hook that allows you to add state to functional components
+import "./App.css";
+import CardList from "./components/card-list/card-list.component";
+import SearchBox from "./components/search-box/search-box.component";
+
+function App() {
+    // https://react.dev/reference/react/useState
+    // useState is a hook that allows you to add state to functional components
+    // It returns an array with two elements: the current state value and a function to update it
+    // set functions, like setSomething(nextState):
+    // The set function returned by useState lets you update the state to a different value and trigger a re-render.
+    // You can pass the next state directly, or a function that calculates it from the previous state
+    const [monsters, setMonsters] = useState([]);
+    const [filteredMonsters, setFilteredMonsters] = useState([]);
+
+    // useEffect is a hook that allows you to perform side effects in functional components
+    // if an empty array is passed, it will run only once after the first render
+    // useEffect is used in this case to fetch data only once when the component mounts
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/users")
+            .then((response) => response.json())
+            .then((data) => {
+                setMonsters(data);
+                setFilteredMonsters(data);
+            });
+    }, []);
+
+    const filterMonsters = (event) => {
+        const searchString = event.target.value.toLocaleLowerCase();
+        const newFilteredMonsters = monsters.filter((monster) =>
+            monster.name.toLocaleLowerCase().includes(searchString)
+        );
+        setFilteredMonsters(newFilteredMonsters);
+    };
+
+    return (
+        <div className="app-container">
+            <h1 className="app-title">Monsters Rolodex</h1>
+            <SearchBox onChangeHandler={filterMonsters} />
+            <CardList monsters={filteredMonsters} />
+        </div>
+    );
+}
+
+export default App;
